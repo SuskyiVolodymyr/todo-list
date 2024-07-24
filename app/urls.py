e0f4f66b5ffb481fa7f544af3621db809b735bf0
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-07-24 10:32:58 +0300

    add task create page and add crispy

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from app.views import index
+from app.views import index, TaskCreateView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("task/create", TaskCreateView.as_view(), name="task-create")
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.views import generic
+
+from app.models import Task
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "app/index.html")
+    tasks = Task.objects.prefetch_related("tag")
+    context = {
+        "tasks": tasks
+    }
+    return render(request, "app/index.html", context)
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
