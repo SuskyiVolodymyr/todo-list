2a307bf996948279b13f45bcaa9fe2dcce3e7798
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-07-24 12:17:23 +0300

    add sorting by status and date created for tasks

--- app/models.py
+++ app/models.py
@@ -8,6 +8,9 @@ class Task(models.Model):
     is_done = models.BooleanField()
     tags = models.ManyToManyField("Tag", related_name="tasks")
 
+    class Meta:
+        ordering = ["is_done", "-created"]
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
